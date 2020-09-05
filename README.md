4a65e0b37abd01a04cdc49efe550872d08169ef5
Mykhailo Pechkurov <mykhailo.pechkurov@here.com>
2020-09-05 15:09:05 +0200

    Initial commit: - added basic spring application

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spring security example
+
+## Tech stack
+- maven
+- java 1.8
+- spring boot
---
+++ guest-services/src/main/java/com/pechkurov/security/services/GuestRepository.java
@@ -0,0 +1,8 @@
+package com.pechkurov.security.services;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+}
