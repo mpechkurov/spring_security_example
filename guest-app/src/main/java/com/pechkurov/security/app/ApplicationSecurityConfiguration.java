83f188645b0294c14c1bbbba76280719a91c9505
Mykhailo Pechkurov <mykhailo.pechkurov@here.com>
2020-09-08 16:59:52 +0200

    Added in memory authentication. - permit access to some pages - create user and assign roles * this example should not be used in real life application

--- guest-app/src/main/java/com/pechkurov/security/app/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/com/pechkurov/security/app/ApplicationSecurityConfiguration.java
@@ -1,9 +1,17 @@
 package com.pechkurov.security.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -19,5 +27,16 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
             .authenticated()
             .and()
             .httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> users = new ArrayList<>();
+        users.add(
+            User.withDefaultPasswordEncoder().username("user1").password("password").roles("USER", "ADMIN").build());
+        users
+            .add(User.withDefaultPasswordEncoder().username("user2").password("foobar").roles("USER", "ADMIN").build());
+        return new InMemoryUserDetailsManager(users);
     }
 }
