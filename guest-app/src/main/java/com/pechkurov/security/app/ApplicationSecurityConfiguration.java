d1c1edc4c76b8c01dd51248d388f36b91be49ed1
mpechkurov <mykhailo.pechkurov@gmail.com>
2020-09-13 16:31:51 +0200

    Update security configuration and fix logout.html page.

--- guest-app/src/main/java/com/pechkurov/security/app/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/com/pechkurov/security/app/ApplicationSecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.pechkurov.security.app.auth.LandonUserDetailService;
 
@@ -54,7 +55,10 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
             .anyRequest()
             .authenticated()
             .and()
-            .httpBasic();
+            .formLogin().loginPage("/login").permitAll()
+            .and()
+            .logout().invalidateHttpSession(true).clearAuthentication(true)
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/logout-success").permitAll();
     }
 
 }
