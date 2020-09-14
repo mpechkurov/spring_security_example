efcf94018741e7ad59aa8fd4d4249f780308fdb4
mpechkurov <mykhailo.pechkurov@gmail.com>
2020-09-14 10:25:10 +0200

    Enable LDAP authentication for 2 users: mpechkurov:password jdoe:foobar

--- guest-app/src/main/java/com/pechkurov/security/app/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/com/pechkurov/security/app/ApplicationSecurityConfiguration.java
@@ -1,9 +1,7 @@
 package com.pechkurov.security.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,26 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import com.pechkurov.security.app.auth.LandonUserDetailService;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Autowired
-    LandonUserDetailService userDetailService;
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailService);
-        provider.setPasswordEncoder(new BCryptPasswordEncoder(11));
-        provider.setAuthoritiesMapper(authoritiesMapper());
-        return provider;
-    }
 
     @Bean
     public GrantedAuthoritiesMapper authoritiesMapper() {
@@ -42,7 +27,15 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
+        auth.ldapAuthentication().userDnPatterns("uid={0},ou=people")
+            .groupSearchBase("ou=groups")
+            .authoritiesMapper(authoritiesMapper())
+            .contextSource()
+            .url("ldap://localhost:8389/dc=pechkurov,dc=com")
+            .and()
+            .passwordCompare()
+            .passwordEncoder(new LdapShaPasswordEncoder())
+            .passwordAttribute("userPassword");
     }
 
     @Override
--- guest-app/src/main/java/com/pechkurov/security/app/auth/AuthGroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pechkurov.security.app.auth;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthGroupRepository extends JpaRepository<AuthGroup, Long> {
-    List<AuthGroup> findByUsername(String username);
-}
